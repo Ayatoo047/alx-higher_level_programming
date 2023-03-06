b88c59c197f2a76b52968e2538b9bb83e30dab05
Ajibade Yakub Adebare <ajibadeyakubadebare@gmail.com>
2023-03-06 13:31:19 -0800

    hhj

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
 print(f"First 3 letters: {word[:2]}")
+print("\n")
 print(f"Last 2 letters: {word[-2:-1]}")
+print("\n")
 print(f"Middle word: {word[1:-2]}")
+print("\n")
